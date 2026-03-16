bc6f82029c75f627004bc087fb1084c96cc42280
zzzyyyuuuuu <emirshn000@gmail.com>
2026-03-16 16:31:12 +0300

    ...

--- install-fedora.sh
+++ install-fedora.sh
@@ -1,17 +1,35 @@
-#!/bash/bin/bash
+#!/bin/bash
 set -e
 
 CYAN='\033[0;36m'
 BLUE='\033[0;34m'
 GREEN='\033[0;32m'
 GRAY='\033[0;90m'
+YELLOW='\033[0;33m'
 NC='\033[0m'
 
 clear
-echo -e "${BLUE}●${NC} ${CYAN}Pure-Flow v1.1.0${NC} ${GRAY}-----------------------------------${NC}"
-echo ""
+echo -e "${BLUE}[*]${NC} ${CYAN}Pure-Flow v1.1.0${NC} ${GRAY}-----------------------------------${NC}"
 
-PACKAGES=(hyprland hyprpaper hyprlock swaync matugen rofi waybar kitty dolphin wlogout)
+GPU_TYPE=$(lspci | grep -iE 'vga|3d')
+
+if echo "$GPU_TYPE" | grep -qi nvidia; then
+    echo -e "${BLUE}[i]${NC} Nvidia GPU detected."
+    if ! rpm -q akmod-nvidia > /dev/null; then
+        sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
+    else
+        echo -e "${GREEN}[+] Nvidia drivers are already installed.${NC}"
+    fi
+elif echo "$GPU_TYPE" | grep -qiE 'amd|ati'; then
+    echo -e "${BLUE}[i]${NC} AMD GPU detected."
+    if ! rpm -q mesa-dri-drivers > /dev/null; then
+        sudo dnf install -y mesa-dri-drivers mesa-vulkan-drivers
+    else
+        echo -e "${GREEN}[+] AMD drivers are already installed.${NC}"
+    fi
+fi
+
+PACKAGES=(hyprland hyprpaper hyprlock swaync rofi waybar kitty dolphin matugen wlogout)
 total=${#PACKAGES[@]}
 count=0
 
@@ -23,19 +41,17 @@ for pkg in "${PACKAGES[@]}"; do
     percent=$((count * 100 / total))
     filled=$((percent / 5))
     empty=$((20 - filled))
-    
-    bar=$(printf "%${filled}s" | tr ' ' '━')
-    spacer=$(printf "%${empty}s" | tr ' ' ' ')
+    bar=$(printf "%${filled}s" | tr ' ' '=')
+    spacer=$(printf "%${empty}s" | tr ' ' '-')
 
-    printf "\r${BLUE}  Deploying ${NC}%-15s ${BLUE}[${bar}${GRAY}${spacer}${BLUE}]${NC} %3d%%" "$pkg" "$percent"
-    
+    printf "\r${BLUE} Deploying ${NC}%-15s ${BLUE}[${bar}${GRAY}${spacer}${BLUE}]${NC} %3d%%" "$pkg" "$percent"
     sudo dnf install -y "$pkg" > /dev/null 2>&1
 done
 
-echo -e "\n\n${GREEN}  ✔ Core components successfully deployed.${NC}"
+echo -e "\n\n${GREEN}[+] Core components successfully deployed.${NC}"
 
 if [ -d "fonts" ]; then
-    echo -e "${BLUE}  ●${NC} Synchronizing fonts from subdirectories..."
+    echo -e "${BLUE}[*]${NC} Synchronizing fonts..."
     mkdir -p ~/.local/share/fonts/pure-fonts
     find fonts -type f \( -name "*.ttf" -o -name "*.otf" \) -exec cp {} ~/.local/share/fonts/pure-fonts/ \;
     fc-cache -fv > /dev/null 2>&1
@@ -47,4 +63,4 @@ cp -r config/* ~/.config/
 chmod +x ~/.config/hypr/scripts/* 2>/dev/null || true
 sed -i "s|/home/$USER/|$HOME/|g" ~/.config/hypr/*.conf 2>/dev/null || true
 
-echo -e "\n${CYAN}  Finished.${NC} ${GRAY}Pure-Flow is ready to use.${NC}"
+echo -e "\n${CYAN}Finished.${NC} ${GRAY}Pure-Flow is ready to use :)${NC}"
