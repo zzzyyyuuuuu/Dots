da35bf7291d9736f598a32c0270ce2db284c4b20
maxchennn <emirshn000@gmail.com>
2026-03-29 09:18:35 +0300

    .

--- config/quickshell/scripts/get_keyboard_layout.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Yeah so... this is REALLY bad! This should be refactored at some point!
-hyprctl -j devices | grep -B 3 '"main": true' | head --lines=1 | sed 's/^ \+//;s/,$//' | sed 's/"//g' | awk -F ': ' '{ print $2 }'
